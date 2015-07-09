cd341b10f7b0642af0f6a3bfaff05f22e03bc73f
jednike <jednike@gmail.com>
2015-07-09 23:50:58 +0600

    CreateProject

---
+++ desktop/src/com/parabells/PTGame/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.parabells.PTGame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.parabells.PTGame.PTGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new PTGame(), config);
+	}
+}
