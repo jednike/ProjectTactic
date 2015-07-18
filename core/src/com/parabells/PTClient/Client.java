b6f1102966e3afaf463d2316e7e95bece4c7b0ac
jednike <jednike@gmail.com>
2015-07-19 01:04:45 +0600

    Забыл

--- core/src/com/parabells/PTClient/Client.java
+++ core/src/com/parabells/PTClient/Client.java
@@ -1,4 +1,70 @@
 package com.parabells.PTClient;
 
-public class Client {
+import com.parabells.PTGame.PTGame;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+
+public class Client implements Runnable{
+
+    private String fuser;
+    private Socket fromserver;
+    private PrintWriter out;
+    private PTGame game;
+
+    public Client(PTGame game){
+        this.game = game;
+    }
+
+    public String getFuser() {
+        return fuser;
+    }
+
+    public void setFuser(String fuser) {
+        this.fuser = fuser;
+    }
+
+    private void init() throws IOException{
+        String IPAdress = "109.120.41.202";
+        fuser = "";
+        fromserver = new Socket(InetAddress.getLocalHost(), 7070);
+
+        BufferedReader in  = new BufferedReader(new	InputStreamReader(fromserver.getInputStream()));
+        out = new PrintWriter(fromserver.getOutputStream(),true);
+
+        fuser = "";
+        try {
+            while ((fuser = in.readLine()) != null ) {
+                if (fuser.equalsIgnoreCase("exit")) break;
+                game.addToInputQueue(fuser);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("BufferListener " + Thread.currentThread().getName() + " Closed");
+
+        out.close();
+        in.close();
+
+        fromserver.close();
+        System.out.println("ClientClosed");
+    }
+
+    public PrintWriter getOut() {
+        return out;
+    }
+
+    @Override
+    public void run() {
+        try {
+            init();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
